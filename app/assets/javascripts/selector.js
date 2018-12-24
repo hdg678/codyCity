a241464ccea93137dc671a434c1b4abbcef8cb7b
jonathanbuchanan <jonathan.russ.buchanan@gmail.com>
2018-12-24 11:59:39 -0500

    Implemented a show/hide selector for the lesson view

--- app/assets/javascripts/selector.js
+++ app/assets/javascripts/selector.js
@@ -9,7 +9,7 @@ function select(option) {
 }
 
 function selectorFor(handler, options) {
-  elements = options.map(function(item, index) { return item.id });
+  var elements = options.map(function(item, index) { return item.id; });
   for (var i = 0; i < options.length; ++i) {
     option = options[i];
     $("#" + option.id).click(function(event) {
@@ -18,4 +18,42 @@ function selectorFor(handler, options) {
       handler(options.find(function(option) { return option.id == event.currentTarget.id; }));
     });
   }
+  $("#" + options[0].id).trigger("click");
+}
+
+function horizontalSelector(options, params) {
+  handler = function(option) {
+    params.form_element.attr("action", option.action);
+    for (var i = 0; i < params.form_inputs.length; ++i) {
+      input = params.form_inputs[i];
+      $("#" + input.id).attr("name", option.attribute + "[" + input.name + "]");
+    }
+
+    if ($("#" + option.id).index() == 0) {
+      params.bottom_element.addClass(params.first_selected);
+    } else {
+      params.bottom_element.removeClass(params.first_selected);
+    }
+  }
+
+  selectorFor(handler, options);
+}
+
+function horizontalSelectorShowHide(options, params) {
+  handler = function(option) {
+    var elements = options.map(function(item, index) { return item.element; });
+    for (var i = 0; i < elements.length; ++i) {
+      $(elements[i]).hide();
+    }
+
+    $(option.element).show();
+
+    if ($("#" + option.id).index() == 0) {
+      params.bottom_element.addClass(params.first_selected);
+    } else {
+      params.bottom_element.removeClass(params.first_selected);
+    }
+  }
+
+  selectorFor(handler, options);
 }
