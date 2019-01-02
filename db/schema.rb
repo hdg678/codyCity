77eba70393e1d579672644729b4782eb10f53c91
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2019-01-01 23:27:42 -0500

    Revert "Flow"

--- db/schema.rb
+++ db/schema.rb
@@ -134,6 +134,13 @@ ActiveRecord::Schema.define(version: 2018_12_03_182940) do
     t.index ["unlock_token"], name: "index_instructors_on_unlock_token", unique: true
   end
 
+  create_table "jwt_blacklists", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jti"], name: "index_jwt_blacklists_on_jti"
+  end
+
   create_table "lessons", id: :serial, force: :cascade do |t|
     t.integer "course_id"
     t.string "title"
