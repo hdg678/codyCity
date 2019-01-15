b431e244ed80ad76d9ed4339aa984d05b28d830f
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2019-01-15 14:05:05 -0500

    wrote successful tests for lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -3,5 +3,5 @@ class Lesson < ApplicationRecord
 
   has_many :exercises, dependent: :destroy
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 200 }
 end
--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class LessonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid lesson" do
+    course = courses(:one)
+    lesson = Lesson.new(title: "title",
+      course: course)
+    assert lesson.valid?
+  end
+
+  test "valid title" do
+    # Title must exist
+    no_title = Lesson.new(title: nil)
+    assert_not no_title.valid?
+    assert_not no_title.errors[:title].empty?
+
+    # Title must be shorter than 200 characters
+    long_title = Lesson.new(title: "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")
+    assert_not long_title.valid?
+    assert_not long_title.errors[:title].empty?
+
+    # A valid title produces no errors
+    valid_title = Lesson.new(title: "This is a valid title.")
+    valid_title.valid?
+    assert valid_title.errors[:title].empty?
+  end
+
+  test "valid order value" do
+    skip
+  end
 end
