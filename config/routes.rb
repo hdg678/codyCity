760d24ec34706ea0c8cf1c8b9f157498a94c3875
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2019-01-18 20:33:21 -0500

    Deleted unused api controllers

--- config/routes.rb
+++ config/routes.rb
@@ -22,14 +22,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'Student', at: 'students', controllers: { registrations: 'api/v1/registrations', sessions: 'api/v1/sessions' }
-      resources :students, only: [:index, :show]
-
       mount_devise_token_auth_for 'Instructor', at: 'instructors', controllers: { registrations: 'api/v1/registrations', sessions: 'api/v1/sessions' }
-      resources :instructors, only: [:index, :show]
-
       mount_devise_token_auth_for 'Developer', at: 'developers', controllers: { registrations: 'api/v1/registrations', sessions: 'api/v1/sessions' }
-      resources :developers, only: [:index, :show]
-
       mount_devise_token_auth_for 'Admin', at: 'admins', controllers: { registrations: 'api/v1/registrations', sessions: 'api/v1/sessions' }
 
       get 'tokens/generate', to: 'organization_user_tokens#generate'
