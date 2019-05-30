e8676c2a6e42387aca9a9d758224d3a03b546e82
Jonathan Buchanan <jonathan.russ.buchanan@gmail.com>
2019-05-29 23:44:39 -0400

    Create new exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -9,9 +9,33 @@ class ExercisesController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+
+  end
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    if @exercise.save
+      redirect_to @exercise
+    else
+      render :new
+    end
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_exercise
     @exercise = Exercise.find(params[:id])
+  end
+
+  def exercise_params
+    params.require(:exercise).permit(:title, :instructions, :lesson_id, :point_value)
   end
 end
